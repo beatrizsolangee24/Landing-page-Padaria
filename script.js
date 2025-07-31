c6fac133a9b75b634ffdfc06996e2da363950dfd
Beatriz Moraes <beatrizsolangee@gmail.com>
2025-07-31 16:14:59 -0300

    Adicionando arquivos com novas alterações

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-$(document).ready(function(){
-    $('#mobile-btn').on('click', function() {
-        $('#mobile_menu').toggleClass('active');
-        $(this).toggleClass('bi-list bi-x');
+  $(document).ready(function () {
+    $('#mobile-btn').on('click', function () {
+      $('#mobile_menu').toggleClass('active');
+      $(this).toggleClass('bi-list bi-x');
     });
-});
 
+    ScrollReveal().reveal('#content-1', {
+      origin: 'left',
+      duration: 2000,
+      distance: '2%' // com aspas
+    });
 
+     ScrollReveal().reveal('#p-frase', {
+      origin: 'left',
+      duration: 2000,
+      distance: '2%' // com aspas
+    });
+       ScrollReveal().reveal('#texto-2-container', {
+      origin: 'right',
+      duration: 2000,
+      distance: '2%' // com aspas
+    });
+           ScrollReveal().reveal('.content-ingredientes-filho', {
+      origin: 'left',
+      duration: 2000,
+      distance: '2%' // com aspas
+    });
+            ScrollReveal().reveal('#final-content-title', {
+      origin: 'left',
+      duration: 2000,
+      distance: '2%' // com aspas
+    });
+
+  });
